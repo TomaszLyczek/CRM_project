d4a9a62ca58c166bc1ea35c7c129cfce1c4a09e4
Tomek Ly <zcinzcen@gmail.com>
2021-05-18 06:17:43 +0200

    Methods added to CompanyService and CompanyRepository. Adding Company and finding by name

--- src/main/java/pl/tl/crm/company/CompanyRepository.java
+++ src/main/java/pl/tl/crm/company/CompanyRepository.java
@@ -2,5 +2,10 @@ package pl.tl.crm.company;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CompanyRepository extends JpaRepository<Company,Long> {
+
+    Optional<Company> findByName(final String name);
 }
--- src/main/java/pl/tl/crm/company/CompanyService.java
+++ src/main/java/pl/tl/crm/company/CompanyService.java
@@ -3,11 +3,16 @@ package pl.tl.crm.company;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class CompanyService
@@ -23,5 +28,22 @@ public class CompanyService
         List<Company> companies = companyRepository.findAll();
 
         return ResponseEntity.ok(objectMapper.writeValueAsString(companies));
+    }
+    @GetMapping(value = "/companies",params = "name")
+    public ResponseEntity getCompany(@Param("name") String name) throws JsonProcessingException {
+        Optional<Company> company = companyRepository.findByName(name);
+        return ResponseEntity.ok(objectMapper.writeValueAsString(company));
+    }
+
+    @PostMapping(value = "/companies")
+    public ResponseEntity addCompany(@RequestBody Company newCompany) throws JsonProcessingException {
+
+        Optional<Company> companyFromDb = companyRepository.findByName(newCompany.getName());
+        if (companyFromDb.isPresent()){
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
+
+        Company savedCompany = companyRepository.save(newCompany);
+        return ResponseEntity.ok(objectMapper.writeValueAsString(savedCompany));
     }
 }
