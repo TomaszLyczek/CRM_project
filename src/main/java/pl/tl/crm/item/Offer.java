6816717242c63e97b2252b79c5367891e275098b
Tomek Ly <zcinzcen@gmail.com>
2021-05-23 15:55:26 +0200

    Offer linked with Company and Item with @ManyToOne

--- src/main/java/pl/tl/crm/item/Offer.java
+++ src/main/java/pl/tl/crm/item/Offer.java
@@ -1,24 +1,29 @@
 package pl.tl.crm.item;
 
-
 import lombok.*;
+import pl.tl.crm.company.Company;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
-@Table(name = "items")
+@Table(name = "offers")
 @AllArgsConstructor
 @NoArgsConstructor
 @RequiredArgsConstructor
 @Getter
 @Setter
 @ToString
-class Item {
+class Offer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Integer id;
     @NonNull
-    String name;
+            @ManyToOne
+    Company company;
+    @ManyToOne
     @NonNull
-    Integer price;
+    Item item;
+
+    Integer discount;
 }
